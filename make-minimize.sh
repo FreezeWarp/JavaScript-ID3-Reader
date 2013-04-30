7f42c352900b59a715c061e5571e209e507d5ab5
FreezeWarp <josephtparsons@gmail.com>
2013-04-29 20:43:18 -0500

    Update minimize.w

--- make-minimize.sh
+++ make-minimize.sh
@@ -3,7 +3,6 @@ CLOSURE_COMPILER=/usr/local/closure-compiler/compiler.jar
 java -jar "$CLOSURE_COMPILER" \
     --compilation_level ADVANCED_OPTIMIZATIONS \
     --js src/stringutils.js \
-    --js src/bufferedbinaryajax.js \
     --js src/filereader.js \
     --js src/id3.js \
     --js src/id3v1.js \
